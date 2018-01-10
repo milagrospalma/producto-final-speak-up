e11073dd2ad0faebbd978ad0843e6157ce26d16d
ruthmz <rmatosfer@gmail.com>
2018-01-10 04:23:13 -0500

    funcionalidad newsfeed

---
+++ js/newsfeed.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  var $write = $('#write');
+  var textarea = $('#textarea1')
+  var content = $('#content')
+
+  $write.on('click', function(e){ 
+      $('#post-box').toggleClass('hide');
+  })
+
+  textarea.on('input', function(event) {
+    if ($(this).val().length > 0) {
+      $('.btn').attr('disabled', false);
+    } else {
+      $('.btn').attr('disabled', true);
+    }
+  });
+  
+  $('.btn').on('click', function(e){
+    event.preventDefault(e)
+    var info = textarea.val();
+    content.prepend('<div class= "border">'+ info + "</div>");
+    textarea.val("")
+    $('.btn').attr('disabled', true);
+  })
+});
