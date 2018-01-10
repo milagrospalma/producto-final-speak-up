c398e9ff070a1f185925881f8b2dbb77e0b84251
ruthmz <rmatosfer@gmail.com>
2018-01-10 18:11:24 -0500

    modificando jquery

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
   });
 
   textarea.on('input', function(event) {
-    if ($(this).val().length > 0 || image) {
+    if ($(this).val().length > 0) {
       $('.btn').attr('disabled', false);
     } else {
       $('.btn').attr('disabled', true);
@@ -22,21 +22,22 @@ $(document).ready(function() {
   $('.btn').on('click', function(e) {
     event.preventDefault(e);
     var info = textarea.val();
-    content.prepend('<div id="post" class= "border">' + info +  '<div class = "center-align"> <img src="" id="picture" alt="" class="hide" width= 220px;></div><div class= "right-align">'+ time +'</div> </div>');
+    content.prepend('<div id="post" class= "border">' + info + '<div class = "center-align"> <img src="" id="picture" alt="" class="hide" width= 220px;></div><div class= "right-align">' + time + '</div> </div>');
     textarea.val('');
     $('#picture').prepend('#post');        
     $('.btn').attr('disabled', true);
-    $('#picture').attr('src', image)
+    $('#picture').attr('src', image);
     $('#picture').removeClass('hide');        
   });
   
-  $('.select-img').change(function () {
+  $('.select-img').change(function() {
     if (this.files && this.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-          image = e.target.result
-        }
-        reader.readAsDataURL(this.files[0]);
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        image = e.target.result;
+        $('.btn').attr('disabled', false);
+      };
+      reader.readAsDataURL(this.files[0]);
     }
-  })
+  });
 });
