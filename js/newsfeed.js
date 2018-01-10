4efd56b710c971fedcfd6d4da323345d96113891
milagrospalma <mnpalma.2@gmail.com>
2018-01-10 18:20:10 -0500

    Juntando vistas con funcionalidad

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   var $write = $('#write');
-  var textarea = $('#textarea1')
-  var content = $('#content')
+  var textarea = $('#textarea1');
+  var content = $('#content');
+  var time = moment().format('LT');
+  console.log(time);
 
-  $write.on('click', function(e){ 
+  $write.on('click', function(e) { 
     $('#post-box').toggleClass('hide');
-  })
+    $('#upload').toggleClass('hide');
+  });
 
   textarea.on('input', function(event) {
     if ($(this).val().length > 0) {
@@ -14,12 +17,27 @@ $(document).ready(function() {
       $('.btn').attr('disabled', true);
     }
   });
-  
-  $('.btn').on('click', function(e){
-    event.preventDefault(e)
+
+  var image;
+  $('.btn').on('click', function(e) {
+    event.preventDefault(e);
     var info = textarea.val();
-    content.prepend('<div class= "border">'+ info + "</div>");
-    textarea.val("")
+    content.prepend('<div id="post" class= "border">' + info + '<div class = "center-align"> <img src="" id="picture" alt="" class="hide" width= 220px;></div><div class= "right-align">' + time + '</div> </div>');
+    textarea.val('');
+    $('#picture').prepend('#post');        
     $('.btn').attr('disabled', true);
-  })
+    $('#picture').attr('src', image);
+    $('#picture').removeClass('hide');        
+  });
+  
+  $('.select-img').change(function() {
+    if (this.files && this.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        image = e.target.result;
+        $('.btn').attr('disabled', false);
+      };
+      reader.readAsDataURL(this.files[0]);
+    }
+  });
 });
