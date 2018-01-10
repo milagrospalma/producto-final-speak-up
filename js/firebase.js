09aead0840a3075439a3da7092ee7e210fbfd0ea
milagrospalma <mnpalma.2@gmail.com>
2018-01-10 17:42:56 -0500

    Añade funcionalidad a vista search y agrega data

--- js/firebase.js
+++ js/firebase.js
@@ -10,3 +10,46 @@ var config = {
 firebase.initializeApp(config);
 // Creando instancia del objeto del proveedor de Google
 var provider = new firebase.auth.GoogleAuthProvider();
+
+// Data
+var data = {
+  'ingles': [
+    {
+      'name': 'Juan',
+      'gender': 'masculino',
+      'email': 'jmendoza@gmail.com',
+      'birthday': '03/10/1996',
+      'country': 'USA',
+      'lengnativo': 'Inglés',
+      'lenglearn': ['Español'],
+      'aboutme': 'Student',
+      'like': ['music', 'technology']
+    }
+  ],
+  'frances': [
+    {
+      'name': 'Eve',
+      'gender': 'femenino',
+      'email': 'evebouch@gmail.com',
+      'birthday': '05/10/1996',
+      'country': 'Francia',
+      'lengnativo': 'Francés',
+      'lenglearn': ['Español'],
+      'aboutme': 'Musician',
+      'like': ['music', 'technology']
+    }
+  ],
+  'italiano': [
+    {
+      'name': 'Astrid',
+      'gender': 'femenino',
+      'email': 'astridmangi@gmail.com',
+      'birthday': '19/03/1996',
+      'country': 'Italia',
+      'lengnativo': 'Italiano',
+      'lenglearn': ['Español'],
+      'aboutme': 'Student',
+      'like': ['music', 'technology']
+    }
+  ]
+};
