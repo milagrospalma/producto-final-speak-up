cdfe950e25aa12c7440a9f8d7147f1dd1e10a25e
ruthmz <rmatosfer@gmail.com>
2018-01-10 17:56:41 -0500

    agregando fotos

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -1,25 +1,42 @@
 $(document).ready(function() {
   var $write = $('#write');
-  var textarea = $('#textarea1')
-  var content = $('#content')
+  var textarea = $('#textarea1');
+  var content = $('#content');
+  var time = moment().format('LT');
+  console.log(time);
 
-  $write.on('click', function(e){ 
+  $write.on('click', function(e) { 
     $('#post-box').toggleClass('hide');
-  })
+    $('#upload').toggleClass('hide');
+  });
 
   textarea.on('input', function(event) {
-    if ($(this).val().length > 0) {
+    if ($(this).val().length > 0 || image) {
       $('.btn').attr('disabled', false);
     } else {
       $('.btn').attr('disabled', true);
     }
   });
-  
-  $('.btn').on('click', function(e){
-    event.preventDefault(e)
+
+  var image;
+  $('.btn').on('click', function(e) {
+    event.preventDefault(e);
     var info = textarea.val();
-    content.prepend('<div class= "border">'+ info + "</div>");
-    textarea.val("")
+    content.prepend('<div id="post" class= "border">' + info +  '<div class = "center-align"> <img src="" id="picture" alt="" class="hide" width= 220px;></div><div class= "right-align">'+ time +'</div> </div>');
+    textarea.val('');
+    $('#picture').prepend('#post');        
     $('.btn').attr('disabled', true);
+    $('#picture').attr('src', image)
+    $('#picture').removeClass('hide');        
+  });
+  
+  $('.select-img').change(function () {
+    if (this.files && this.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          image = e.target.result
+        }
+        reader.readAsDataURL(this.files[0]);
+    }
   })
 });
