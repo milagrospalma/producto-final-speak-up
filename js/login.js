4ccb21a9c7d35b12efc03fa59f3082a97e485434
ruthmz <rmatosfer@gmail.com>
2018-01-12 04:21:13 -0500

    corrigiendo indentación y agregando comentarios

--- js/login.js
+++ js/login.js
@@ -1,3 +1,3 @@
-$(document).ready(function(){
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
-})
+$(document).ready(function() {
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+});
--- js/newsfeed.js
+++ js/newsfeed.js
@@ -3,16 +3,18 @@ $(document).ready(function() {
   var textarea = $('.text-post');
   var content = $('.content');
   var time = moment().format('LT');
-  var select =  $('.select-img');
+  var select = $('.select-img');
   var image;
 
-  $('.info').append(localStorage.post)
+  $('.info').append(localStorage.post);
 
-  $write.on('click', function(e) { 
+  // event to write a post
+  $write.on('click', function(event) { 
     $('#post-box').toggleClass('hide');
     $('#upload').toggleClass('hide');
   });
 
+  // event to activate the buttton
   textarea.on('input', function(event) {
     if ($(this).val().length > 0) {
       $('.btn').attr('disabled', false);
@@ -21,53 +23,29 @@ $(document).ready(function() {
     }
   });
 
-
-  $('.btn-submit').on('click', function(e) {
-    event.preventDefault(e);
+  // submitting a post
+  $('.btn-submit').on('click', function(event) {
+    event.preventDefault(event);
     var info = textarea.val();
     content.prepend('<div id="post" class="border z-depth-2">' + info + '<div class = "center-align"> <img src="" id="picture" alt="" class="hide" width= 220px;></div><div class= "right-align">' + time + '</div> </div>');
     textarea.val('');
-    $('.img-info').val('')
+    $('.img-info').val('');
     $('#picture').prepend('#post');        
     $('#picture').attr('src', image);
     $('#picture').removeClass('hide');
     image = '';
     localStorage.post = content.html();
-
   });
-  
+
+  // event to select an image  
   $('.select-img').change(function() {
     if (this.files && this.files[0]) {
       var reader = new FileReader();
-      reader.onload = function(e) {
+      reader.onload = function(event) {
         image = e.target.result;   
         $('.btn-submit').attr('disabled', false);     
       };
       reader.readAsDataURL(this.files[0]);
     }
   });
-  // $('.btn-submit').on('click', function(e) {
-  //   $('.select-img').val('');
-  //   event.preventDefault(e);
-  //   var info = textarea.val();
-  //   content.prepend('<div id="post" class= "border z-depth-2">' + info + '<div class = "center-align"> <img src="" id="picture" alt="" class="hide" width= 220px;></div><div class= "right-align"> <hr class= "gray">' + time + '</div> </div>');
-  //   textarea.val('');
-  //   $('.img-info').val('');
-  //   $('#picture').prepend('#post');        
-  //   $('.btn-submit').attr('disabled', true);
-  //   $('#picture').attr('src', image);
-  // });
-  
-  // $('.select-img').change(function() {
-  //   if (this.files && this.files[0]) {
-  //     var reader = new FileReader();
-  //     reader.onload = function(e) {
-  //       image = e.target.result;
-  //       $('.btn-submit').attr('disabled', false);
-  //       $('#picture').removeClass('hide');        
-  //     };
-  //     reader.readAsDataURL(this.files[0]);
-
-  //   }
-  // });
 });
