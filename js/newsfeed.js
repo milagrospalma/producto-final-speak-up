a8f1f4c32e993664afebfced4413c0cd1b1ad751
ruthmz <32310744+ruthmz@users.noreply.github.com>
2018-01-12 10:53:21 -0500

    Corrigiendo funcionalidad

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
   $('.info').append(localStorage.post);
 
   // event to write a post
-  $write.on('click', function(event) { 
+  $write.on('click', function(e) { 
     $('#post-box').toggleClass('hide');
     $('#upload').toggleClass('hide');
   });
@@ -24,8 +24,8 @@ $(document).ready(function() {
   });
 
   // submitting a post
-  $('.btn-submit').on('click', function(event) {
-    event.preventDefault(event);
+  $('.btn-submit').on('click', function(e) {
+    event.preventDefault(e);
     var info = textarea.val();
     content.prepend('<div id="post" class="border z-depth-2">' + info + '<div class = "center-align"> <img src="" id="picture" alt="" class="hide" width= 220px;></div><div class= "right-align">' + time + '</div> </div>');
     textarea.val('');
@@ -35,13 +35,14 @@ $(document).ready(function() {
     $('#picture').removeClass('hide');
     image = '';
     localStorage.post = content.html();
+    $('.btn-submit').attr('disabled', true);     
   });
 
   // event to select an image  
   $('.select-img').change(function() {
     if (this.files && this.files[0]) {
       var reader = new FileReader();
-      reader.onload = function(event) {
+      reader.onload = function(e) {
         image = e.target.result;   
         $('.btn-submit').attr('disabled', false);     
       };
