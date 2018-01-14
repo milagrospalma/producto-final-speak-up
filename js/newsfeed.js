ffed6d120229ccfd111bbbf9673ad6c609841f0b
NatalyCortez <flyingchair_pe12@hotmail.com>
2018-01-14 02:56:14 -0500

    implementando estructura,agregando estilos y eventos con jQuery

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -1,20 +1,12 @@
 $(document).ready(function() {
   var $write = $('#write');
-  var textarea = $('.text-post');
-  var content = $('.content');
-  var time = moment().format('LT');
-  var select = $('.select-img');
-  var image;
+  var textarea = $('#textarea1')
+  var content = $('#content')
 
-  $('.info').append(localStorage.post);
-
-  // event to write a post
-  $write.on('click', function(e) { 
+  $write.on('click', function(e){ 
     $('#post-box').toggleClass('hide');
-    $('#upload').toggleClass('hide');
-  });
+  })
 
-  // event to activate the buttton
   textarea.on('input', function(event) {
     if ($(this).val().length > 0) {
       $('.btn').attr('disabled', false);
@@ -22,31 +14,12 @@ $(document).ready(function() {
       $('.btn').attr('disabled', true);
     }
   });
-
-  // submitting a post
-  $('.btn-submit').on('click', function(e) {
-    event.preventDefault(e);
+  
+  $('.btn').on('click', function(e){
+    event.preventDefault(e)
     var info = textarea.val();
-    content.prepend('<div id="post" class="border z-depth-2">' + info + '<div class = "center-align"> <img src="" id="picture" alt="" class="hide" width= 220px;></div><div class= "right-align">' + time + '</div> </div>');
-    textarea.val('');
-    $('.img-info').val('');
-    $('#picture').prepend('#post');        
-    $('#picture').attr('src', image);
-    $('#picture').removeClass('hide');
-    image = '';
-    localStorage.post = content.html();
-    $('.btn-submit').attr('disabled', true);     
-  });
-
-  // event to select an image  
-  $('.select-img').change(function() {
-    if (this.files && this.files[0]) {
-      var reader = new FileReader();
-      reader.onload = function(e) {
-        image = e.target.result;   
-        $('.btn-submit').attr('disabled', false);     
-      };
-      reader.readAsDataURL(this.files[0]);
-    }
-  });
+    content.prepend('<div class= "border">'+ info + "</div>");
+    textarea.val("")
+    $('.btn').attr('disabled', true);
+  })
 });
